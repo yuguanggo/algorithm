bb4898b4dd2251129e628755ee99cc192657603e
余光 <55458838@qq.com>
2019-01-11 18:30:44 +0800

    test

--- search/main.go
+++ search/main.go
@@ -1,15 +1,10 @@
 package main
 
-import (
-	"algorithm/sort/sorttesthelper"
-	"fmt"
-	"algorithm/search/binarysearch"
-)
-
 func main() {
-	var arr = sorttesthelper.GenerateNearlyOrderedArray(100000, 0)
-	index := binarysearch.Find(arr, 1)
-	fmt.Println(index)
-	index2 := binarysearch.RecursionFind(arr, 0,len(arr)-1,1)
-	fmt.Println(index2)
+	//var arr = sorttesthelper.GenerateNearlyOrderedArray(100000, 0)
+	//index := binarysearch.Find(arr, 1)
+	//fmt.Println(index)
+	//index2 := binarysearch.RecursionFind(arr, 0,len(arr)-1,1)
+	//fmt.Println(index2)
+
 }
