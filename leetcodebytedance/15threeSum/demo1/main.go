d615a8fa3b51c2789facc9bc2e832209c74a0f02
余光 <yuguanggo@qq.com>
2019-08-26 21:08:52 +0800

    com

--- leetcodebytedance/15threeSum/demo1/main.go
+++ leetcodebytedance/15threeSum/demo1/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"sort"
+)
 
 func threeSum(nums []int) [][]int {
 	res:=make([][]int,0)
@@ -8,24 +12,35 @@ func threeSum(nums []int) [][]int {
 	if len(nums)==0{
 		return res
 	}
+	sort.Ints(nums)
+	fmt.Println(nums)
 	path=append(path,nums[0])
-	dfs(nums,1,1,nums[0],path,&res)
+	v:=make(map[string]bool)
+	dfs(nums,1,1,nums[0],path,v,&res)
 	return res
 }
 
-func dfs(nums []int,index int,pos int,sum int,path []int,res *[][]int)  {
+func dfs(nums []int,index int,pos int,sum int,path []int,v map[string]bool,res *[][]int)  {
 	if index>=3{
 		if sum==0{
 			p:=make([]int,len(path))
 			copy(p,path)
-			*res=append(*res,p)
+			str:=""
+			for j:=0;j<3;j++{
+				str+=strconv.Itoa(p[j])
+			}
+			if !v[str]{
+				v[str]=true
+				*res=append(*res,p)
+			}
+
 		}
 		return
 	}
 	for i:=pos;i<len(nums);i++{
 		sum=sum+nums[i]
 		path=append(path,nums[i])
-		dfs(nums,index+1,i+1,sum,path,res)
+		dfs(nums,index+1,i+1,sum,path,v,res)
 		path=path[0:len(path)-1]
 		sum=sum-nums[i]
 	}
