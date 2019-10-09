b79595087c2b86000d480a4a283a3fd8343cf1e4
余光 <yuguanggo@qq.com>
2019-10-09 20:55:23 +0800

    com

---
+++ liubobo/46permute/demo1/main.go
@@ -0,0 +1,19 @@
+package main
+
+
+func permute(nums []int) [][]int {
+
+}
+
+func helper(nums []int,path []int,visited map[int]bool,res *[][]int)  {
+ 	if len(path)==len(nums){
+ 		p:=make([]int,len(path))
+ 		copy(p,path)
+
+	}
+}
+
+
+func main() {
+	
+}
